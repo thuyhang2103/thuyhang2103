d5712cfd15da2ea36a9c14dcab765fddb29cd49b
thuyhang2103 <nguyenthuyhang8407@gmail.com>
2025-04-18 13:18:15 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+/*// JavaScript Document
+$(document).ready(function(){*/
+	
+$(document).ready(function(){
+  $(".btn-close").click(function(){  
+    $(".quangcao").hide();
+	$(".overlay").hide();
+  });
+
+ });
+
+	
